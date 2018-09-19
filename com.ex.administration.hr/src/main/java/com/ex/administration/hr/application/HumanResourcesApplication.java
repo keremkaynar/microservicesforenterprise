d1107d86d8d79d0f786e2cf315dc4e7d6315faac
Kerem Kaynar <kerem.kaynar@yahoo.com>
2018-09-19 09:48:02 -0400

    Reading configuration data from conf. service continued.

--- com.ex.administration.hr/src/main/java/com/ex/administration/hr/application/HumanResourcesApplication.java
+++ com.ex.administration.hr/src/main/java/com/ex/administration/hr/application/HumanResourcesApplication.java
@@ -2,7 +2,9 @@ package com.ex.administration.hr.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HumanResourcesApplication {
 	public static void main(String[] args) {
